3126e270106eb013a24faeddae02050d94a2eb4d
zoeyw <wang.zhe2@husky.neu.edu>
2021-11-17 22:46:23 -0800

    fix the font and board place

--- src/result/Result.jsx
+++ src/result/Result.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import{localStorageKey} from '../helpers/constants'
+import './Result.css';
 
 export default function Result() {
 	const queryString = window.location.search;
 	const urlParams = new URLSearchParams(queryString);
     const winner = urlParams.get('winner');
     localStorage.removeItem(localStorageKey);
-	return <div>Winner is {winner}</div>;
+	return <div class ="result">Winner is {winner}!!!</div>;
 }
